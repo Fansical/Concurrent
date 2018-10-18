e83590426a302b6d7cc45b21344bb2a75696c759
Lawrence <751074877@qq.com>
2018-10-18 21:45:49 +0800

    idea commit

--- src/main/java/com/mytest/concurrence/ConcurrenceApplication.java
+++ src/main/java/com/mytest/concurrence/ConcurrenceApplication.java
@@ -8,5 +8,6 @@ public class ConcurrenceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConcurrenceApplication.class, args);
+		System.out.println("Hello!");
 	}
 }
