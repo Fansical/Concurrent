5252fe5468e2a6cc23bfbbf40d7e115b4f4cbc98
Lawrence <751074877@qq.com>
2018-10-19 16:38:21 +0800

    add annotations

--- src/main/java/com/mytest/concurrence/ConcurrenceApplication.java
+++ src/main/java/com/mytest/concurrence/ConcurrenceApplication.java
@@ -8,6 +8,5 @@ public class ConcurrenceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConcurrenceApplication.class, args);
-		System.out.println("Hello!");
 	}
 }
